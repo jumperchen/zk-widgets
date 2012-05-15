d849d83388d71c5422e5a0e14f7cf4fc3ca16d43
jimmyshiau73@gmail.com <jimmyshiau73@gmail.com@7d39aa96-b64c-91a0-a5ce-e5eb1300aa45>
2012-05-15 08:59:39 +0000

    update demo data

---
+++ zkfrozengrid/frozengriddemo/src/main/java/org/zkoss/frozendemo/Score.java
@@ -0,0 +1,154 @@
+package org.zkoss.frozendemo;
+
+public class Score {
+	private String player;
+	private int no;
+	private String date;
+	private String time;
+	private String location;
+	private String opponent;
+	private int ab;
+	private int r;
+	private int h;
+	private int b1;
+	private int b2;
+	private int b3;
+	private int hr;
+	private int rbi;
+	private int so;
+	private int bb;
+	private int sac;
+	private int hp;
+	private int po;
+	private int a;
+	private int e;
+	
+	
+	
+	public String getPlayer() {
+		return player;
+	}
+	public void setPlayer(String player) {
+		this.player = player;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getOpponent() {
+		return opponent;
+	}
+	public void setOpponent(String opponent) {
+		this.opponent = opponent;
+	}
+	public int getAb() {
+		return ab;
+	}
+	public void setAb(int ab) {
+		this.ab = ab;
+	}
+	public int getR() {
+		return r;
+	}
+	public void setR(int r) {
+		this.r = r;
+	}
+	public int getH() {
+		return h;
+	}
+	public void setH(int h) {
+		this.h = h;
+	}
+	public int getB1() {
+		return b1;
+	}
+	public void setB1(int b1) {
+		this.b1 = b1;
+	}
+	public int getB2() {
+		return b2;
+	}
+	public void setB2(int b2) {
+		this.b2 = b2;
+	}
+	public int getB3() {
+		return b3;
+	}
+	public void setB3(int b3) {
+		this.b3 = b3;
+	}
+	public int getHr() {
+		return hr;
+	}
+	public void setHr(int hr) {
+		this.hr = hr;
+	}
+	public int getRbi() {
+		return rbi;
+	}
+	public void setRbi(int rbi) {
+		this.rbi = rbi;
+	}
+	public int getSo() {
+		return so;
+	}
+	public void setSo(int so) {
+		this.so = so;
+	}
+	public int getBb() {
+		return bb;
+	}
+	public void setBb(int bb) {
+		this.bb = bb;
+	}
+	public int getSac() {
+		return sac;
+	}
+	public void setSac(int sac) {
+		this.sac = sac;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public int getPo() {
+		return po;
+	}
+	public void setPo(int po) {
+		this.po = po;
+	}
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getE() {
+		return e;
+	}
+	public void setE(int e) {
+		this.e = e;
+	}
+}
